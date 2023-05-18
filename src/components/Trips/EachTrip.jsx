1b094da5ef1c8443bd5f3652b0a3e738182cd6e8
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2023-05-18 11:11:33 -0500

    working on delete edit each trip

--- src/components/Trips/EachTrip.jsx
+++ src/components/Trips/EachTrip.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-
+import { useHistory } from 'react-router-dom';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 function EachTrip() {
 
+    const history = useHistory();
+    const dispatch = useDispatch();
 
-// const dispatch = useDispatch();
-
+// pushes to the categories page
+const showCategories = () => {
+    history.push(`/categories/${props.trip.id}`);
+};
 
 
 
 return (
 
 
-<>
+<li
+
+    onClick={showCategories}>
 
 
-
-
-</>
+</li>
 
 
 
